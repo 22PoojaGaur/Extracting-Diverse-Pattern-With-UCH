55491dc67a3e0b60295f637d8779c860607b97f0
22PoojaGaur <poojagaur2293@gmail.com>
2019-07-15 21:02:34 +0530

    files added for concept hierarchy data cleaning.

---
+++ src/concept_hierarchy_data_cleaner.py
@@ -0,0 +1,21 @@
+"""
+author @22PoojaGaur
+
+This file takes tesco provided concept hierarchy dataset and cleans it.
+
+Input format -
+
+num.[item-name]
+{PATH=}[Item-path-separated-by-/]
+
+Output format -
+
+[item-name]
+[item-path-separated-by-spaces]
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
