eefab2e9087a906d6dc3348a997f6ed803851fc3
22PoojaGaur <poojagaur2293@gmail.com>
2019-07-18 21:43:34 +0530

    file added for data modified in concept hierarchy.

--- src/concept_hierarchy_data_cleaner.py
+++ src/concept_hierarchy_data_cleaner.py
@@ -14,8 +14,39 @@ Output format -
 [item-path-separated-by-spaces]
 """
 
+import sys
+
 def main():
-    pass
+    file_name = sys.argv[1]
+    out_file_name = sys.argv[2]
+    line = ''
+
+    try:
+    	fin = open(file_name, 'r')
+	except:
+		print ('Enter Correct File Name')
+		sys.exit()
+	fout = open(out_file_name, 'w')
+
+	is_item_line=1
+	for line in f.readlines
+		if is_item_line:
+			it_name = line.split('.')[1].strip()
+			fout.write(it_name.replace(' ', '_') + '\n')
+			is_item_line = 0
+		else:
+			full_path = line.split('=')[1]
+			path_array= full_path.split('/')
+
+			for category in path_array:
+					fout.write(category.strip().replace('', '_') + ' ')
+			fout.write('\n\n')
+			is_item_line = 1
+
+
+	fin.closed()
+	fout.closed()
+
 
 if __name__ == '__main__':
     main()
